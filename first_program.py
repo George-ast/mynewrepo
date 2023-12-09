36f02651a3e29473f54668bc82150da8826eb20b
George-ast <george.astash@gmail.com>
2023-12-09 16:25:43 +0200

    changes

--- first_program.py
+++ first_program.py
@@ -1,4 +1,6 @@
-"""home"""
-
-genres_of_music = "heavy metal"
+# first_program.py
+"""home."""
+genres_of_music = 'heavy metal'
 print(genres_of_music)
+print(id(genres_of_music))
+del genres_of_music
