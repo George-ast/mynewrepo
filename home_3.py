e65dbdef87a63df4caf3a61c36ccd885687db240
George-ast <george.astash@gmail.com>
2023-12-11 13:31:19 +0200

    '005'

---
+++ home_3.py
@@ -0,0 +1,15 @@
+"""old_text."""
+
+symbol1 = 'ø'
+symbol2 = 'å'
+symbol3 = 'Æ'
+
+t2 = ('Automatisering akselererer, {0}syeblikket da roboter'
+    'vil erobre planeten v{1}. ({2})').format(symbol1, symbol2, symbol3)
+print(t2)
+
+t3 = (f'Automatisering akselererer, {symbol1}syeblikket da roboter'
+    f'vil erobre planeten v{symbol2} ({symbol3})')
+print(t3)
+if t2 == t3:
+    print('success')
