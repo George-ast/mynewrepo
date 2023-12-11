2188ba3879445704b647faf0a70d30aa3df046de
George-ast <george.astash@gmail.com>
2023-12-11 17:25:18 +0200

    changes

--- home3.3.py
+++ home3.3.py
@@ -5,7 +5,7 @@ symbol2 = 'å'
 symbol3 = 'Æ'
 
 t2 = ('Automatisering akselererer, {0}syeblikket da roboter'
-    'vil erobre planeten v{1}. ({2})').format(symbol1, symbol2, symbol3)
+    'vil erobre planeten v{1} ({2})').format(symbol1, symbol2, symbol3)
 print(t2)
 
 t3 = (f'Automatisering akselererer, {symbol1}syeblikket da roboter'
