38b595f12a2e004a1e61e8cd59cc9dfc485a55ac
George-ast <george.astash@gmail.com>
2023-12-17 22:15:31 +0200

    'fix'

--- home4.1.py
+++ home4.1.py
@@ -2,13 +2,7 @@
 """
 
 
-def count_vowels(text):
-    vowels = 'aeiouAEIOU'
-    vowels_count = {vowel: text.count(vowel) for vowel in vowels}
-    return vowels_count
-
 poem_text = """I wandered lonely as a cloud.
-
 That floats on high o'er vales and hills,
 When all at once I saw a crowd,
 A host, of golden daffodils;
@@ -21,31 +15,48 @@ Along the margin of a bay:
 Ten thousand saw I at a glance,
 Tossing their heads in sprightly dance.
 """
+
+
+def count_vowels(text):
+    """Сounting the number of each vowel in this string.
+    """
+
+    text = text.lower()
+    vowels = 'aeiou'
+    vowels_count = {vowel: text.count(vowel) for vowel in vowels}
+    return vowels_count
+
+
 result = count_vowels(poem_text)
 
 print(f' | {"Vowel":^5} | {"Count":^5} | ')
+
+
 print('-' * 19)
+
 
 for vowel, count in result.items():
     print(f' | {vowel:^5} | {count:^5} | ')
+
+
 print('-' * 19)
 
-def change_string(inputstring):
+
+def change_string(input_string):
+    """Replaces vowels in a string.
+    """
     vowels = {
         'a': 'à',
         'e': 'é',
         'i': 'í',
         'o': 'ó',
         'u': 'ú',
-        'A': 'À',
-        'E': 'É',
-        'I': 'Í',
-        'O': 'Ó',
-        'U': 'Ú'
     }
+
     for key in vowels.keys():
-        inputstring = inputstring.replace(key, vowels[key])
-    print(inputstring)
+        input_string = input_string.replace(key, vowels[key])
+    print(input_string)
+
 
 count_vowels(poem_text)
 change_string(poem_text)
