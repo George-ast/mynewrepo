08b4716dc5fa15b774f6b8de9bdc21cdcd878118
George-ast <george.astash@gmail.com>
2024-01-07 21:50:34 +0200

    add home

---
+++ home7.1.py
@@ -0,0 +1,25 @@
+"""If-elif-else.
+
+#given values w,x,y,z
+#write if-elif-else statement that will
+search minimum value and print smth aka "'y' is minimum value'
+#where 'y' is variable name (not value)
+#advice use python debugger and different values to check your algorithm
+#optionally you can check your algorithm somehow with assert statements
+w, x, y, z = 100, 200, 40, 300
+"""
+
+w, x, y, z = 100, 200, 40, 300
+
+"""
+Checking each condition whether the number is the smallest.
+"""
+
+if (x < w) and (x < y) and (x < z):
+    print('x is minimum value')
+elif (y < w) and (y < x) and (y < z):
+    print('y is minimum value')
+elif (w < x) and (w < y) and (w < z):
+    print('w is minimum value')
+else:
+    print('z is minimum value')
